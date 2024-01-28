8fdfbfd2b3804006a19c83c61a64af222984bbf9
ucctn <chetan.singhal@ucertify.com>
2024-01-28 13:07:53 -0800

    Added Convert to Celsius

--- temperatureConversion/src/temperatureConversion/TemperatureCConversion.java
+++ temperatureConversion/src/temperatureConversion/TemperatureCConversion.java
@@ -3,11 +3,43 @@
 //Temperature Conversion
 
 package temperatureConversion;
+import java.util.Scanner;
 
 public class TemperatureCConversion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Private vars
+	Double celsius;
+	Double fahrenheit;
+	
+	// input temperature
+	public void getFahrenheit()
+	{
+		Scanner tempObj = new Scanner(System.in);
+		System.out.println("Enter degrees in Fahrenheit: ");
+		
+		fahrenheit = tempObj.nextDouble();
+		
+		tempObj.close();
+	}
+	
+	public void convertFahrenheit()
+	{
+		getFahrenheit();
+		celsius = (fahrenheit - 32)*(5/9);
+	}
+	
+	public void printCelsius()
+	{
+		convertFahrenheit();
+		System.out.println("Degrees in Celsius: "+celsius);
+	}
+	
+//----------------------------------------------------------------------------
+	public static void main(String[] args)
+	{
+		TemperatureCConversion temp = new TemperatureCConversion();
+		
+		temp.printCelsius();
 
 	}
 
